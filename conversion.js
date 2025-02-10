f403deba4a28d229a76afd1846dc5810d3ae521b
Singh-Ashutosh13999 <ashutosh13999@gmail.com>
2025-02-10 17:46:23 +0530

    Update

--- conversion.js
+++ conversion.js
@@ -6,8 +6,15 @@ console.log(typeof(y))//This same as upper command but typeof is used as a funct
 
 /*let z="33abc"*/
 /*let z=null//Its show 0 value in console.*/
-let z=undefined//NAn same as lower statement.
+let z=undefined //NAn same as lower statement.
 
+/*If we use boolean values like true and false its Print 1 and 0.  */ 
 let zz=Number(z)
 console.log(typeof zz)//Its show number in console but in value of z is containt both number & string so its not good nature.
 console.log(zz)//Its show Nan Not a Number value because its hold number and string both.
+/* "33"= 33 print 
+33= 33
+"33abn"=Nan
+true=1
+
+*/
