42fbcc197caf65a186b6c4037e842115885ff81d
Singh-Ashutosh13999 <ashutosh13999@gmail.com>
2025-02-10 16:16:54 +0530

    update

--- conversion.js
+++ conversion.js
@@ -4,7 +4,10 @@ let y="33"
 console.log(typeof y)//In this command typeof used as a keyword(predefind).
 console.log(typeof(y))//This same as upper command but typeof is used as a function.
 
-let z="33abc"
+/*let z="33abc"*/
+/*let z=null//Its show 0 value in console.*/
+let z=undefined//NAn same as lower statement.
+
 let zz=Number(z)
 console.log(typeof zz)//Its show number in console but in value of z is containt both number & string so its not good nature.
 console.log(zz)//Its show Nan Not a Number value because its hold number and string both.
