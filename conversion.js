79ab9ff13c6254e6fd6f2ceee65479a0b84af6bf
Singh-Ashutosh13999 <ashutosh13999@gmail.com>
2025-02-10 15:15:25 +0530

    onversion

---
+++ conversion.js
@@ -0,0 +1,10 @@
+let xx=33
+console.log(typeof xx)// typeof Function is used for FInd out the type of the value like string number  etc.
+let y="33"
+console.log(typeof y)//In this command typeof used as a keyword(predefind).
+console.log(typeof(y))//This same as upper command but typeof is used as a function.
+
+let z="33abc"
+let zz=Number(z)
+console.log(typeof zz)//Its show number in console but in value of z is containt both number & string so its not good nature.
+console.log(zz)//Its show Nan Not a Number value because its hold number and string both.
---
+++ variable.js
@@ -0,0 +1,13 @@
+
+console.log("Ashu")
+
+/*NOt use Var in  [production ] 
+  always use let in place of Var because of Scope .
+*/
+  /* If U want to print multiple statment in one console keyword then
+      use table with console like console.table([x,y,z,q]) 
+  */
+console.log(typeof "Ashu") //type is string
+console.log(typeof 5)
+console.log(typeof undefined)//Undefind type is always is undefind
+console.log(typeof null)//Its show Object in console.
