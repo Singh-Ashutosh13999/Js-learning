d0042b0aed1241ec7008c6d8254b8daf00748f3c
Singh-Ashutosh13999 <ashutosh13999@gmail.com>
2025-02-11 16:13:19 +0530

    Operation

--- conversion.js
+++ conversion.js
@@ -1,17 +1,17 @@
 let xx=33
-console.log(typeof xx)// typeof Function is used for FInd out the type of the value like string number  etc.
+//console.log(typeof xx)// typeof Function is used for FInd out the type of the value like string number  etc.
 let y="33"
-console.log(typeof y)//In this command typeof used as a keyword(predefind).
-console.log(typeof(y))//This same as upper command but typeof is used as a function.
+//console.log(typeof y)//In this command typeof used as a keyword(predefind).
+//console.log(typeof(y))//This same as upper command but typeof is used as a function.
 
 /*let z="33abc"*/
 /*let z=null//Its show 0 value in console.*/
 let z=undefined //NAn same as lower statement.
 
 /*If we use boolean values like true and false its Print 1 and 0.  */ 
-let zz=Number(z)
-console.log(typeof zz)//Its show number in console but in value of z is containt both number & string so its not good nature.
-console.log(zz)//Its show Nan Not a Number value because its hold number and string both.
+//let zz=Number(z)
+//console.log(typeof zz)//Its show number in console but in value of z is containt both number & string so its not good nature.
+//console.log(zz)//Its show Nan Not a Number value because its hold number and string both.
 /* "33"= 33 print 
 33= 33
 "33abn"=Nan
@@ -21,3 +21,12 @@ false=0
 "String(Name )"=true
 
 */
+
+/*  *********OPERATION******* */
+console.log(1+"2")//its show value in string formate
+console.log("1" +2)//its show value in string formate
+//But If u used 3 argument in console log then its work on the basis of the forst argument charecter like... if 2 number and then stirng then firs add first 2 value then merge string viseversa first is string then its all value work as a string given below the examples.
+console.log(2+1+"2")// Its show 32 
+console.log("2"+1+3)
+
+
